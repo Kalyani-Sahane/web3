524e403d3c01c76b8c0036fced38c45bfd755a65
Kalyani Sahane <kalyanisahane03@gmail.com>
2024-10-14 22:18:49 +0530

    com

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,7 @@
 require("@matterlabs/hardhat-zksync-solc");
-require('dotenv').config(); // Make sure to load environment variables
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  defaultNetwork: 'sepolia', // Set the default network for Hardhat
   zksolc: {
     version: "1.3.9",
     compilerSource: "binary",
@@ -26,10 +24,6 @@ module.exports = {
       chainId: 324,
       zksync: true,
     },
-    sepolia: { // Configuring Sepolia separately
-      url: 'https://rpc.ankr.com/sepolia',
-      accounts: [`0x${process.env.PRIVATE_KEY}`],
-    },
   },
   paths: {
     artifacts: "./artifacts-zk",
@@ -39,6 +33,14 @@ module.exports = {
   },
   solidity: {
     version: "0.8.17",
+    defaultNetwork: 'sepolia',
+    networks: {
+      hardhat: {},
+      sepolia: {
+        url: 'https://rpc.ankr.com/zksync_era_sepolia',
+        accounts: [`0x${process.env.PRIVATE_KEY}`],
+      }
+    },
     settings: {
       optimizer: {
         enabled: true,
